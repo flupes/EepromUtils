95949593a78329e717bb895ce56139eaa9247a79
Lorenzo Flueckiger <lorenzo@flupes.org>
2011-05-15 10:45:39 -0700

    improved test for ring buffer

--- tests/eepromRingBufferTest.cpp
+++ tests/eepromRingBufferTest.cpp
@@ -11,20 +11,50 @@
 EepromRingBuffer ring(START_ADDR, BUFFER_SZ, DATA_SZ, ENDURANCE);
 int number;
 
+uint16_t index(int i)
+{
+  uint16_t dataSize = 2;
+  uint16_t bufferLength = 12;
+  if ( i < 0 ) {
+    i = -dataSize*i;
+    i = i % bufferLength;
+    return bufferLength - i;
+  }
+  else {
+    return (i*dataSize)%bufferLength;
+  }
+}
+
 void setup()
 {
-  // ring.clear();
-  delay(12000);
-  number=1;
   Serial.begin(9600);
+  // for (int i=0; i<BUFFER_SZ; i++) {
+  //   Serial.print((-2*i)%12, DEC);
+  //   Serial.print(" -- i="); 
+  //   Serial.print(i, DEC);
+  //   Serial.print(" + ");
+  //   Serial.print(index(i), DEC);
+  //   Serial.print(" / - ");
+  //   Serial.println(index(-i), DEC);
+  // }
+  // Let a chance to open a serial console before the program 
+  // actually starts to write data.
+  delay(9000);
+  number=1;
+  Serial.print("---- Ring Buffer Storage Size = ");
+  Serial.print(ring.storageSize(), DEC);
+  Serial.print(" | buffer size = ");
+  Serial.println(ring.bufferSize(), DEC);
+#ifndef NDEBUG
+  Serial.println("==== Initial Eeprom State ====");
   SafeEeprom::show(START_ADDR, ring.storageSize());  
-  ring.rotate(1);
+#endif
 }
 
 void loop()
 {
+  Serial.print("==== Iteration: ");
   Serial.println(number, DEC);
-  SafeEeprom::show(START_ADDR, ring.storageSize());
   if ( number % 5 == 0 ) {
     ring.rotate(2);
     number += 2;
@@ -33,6 +63,25 @@ void loop()
     ring.push((void *)&number);
     number++;
   }
+
+#ifndef NDEBUG
+  SafeEeprom::show(START_ADDR, ring.storageSize());
+#endif
+
+  Serial.print("---- ring buffer index=");
+  Serial.println(ring.currentIndex(), DEC);
+
+  int tmp;
+  for (int i=0; i<BUFFER_SZ; i++) {
+    ring.get(i, (void *)&tmp);
+    Serial.print("  i=");
+    Serial.print(i, DEC);
+    Serial.print(" -> number=");
+    Serial.println(tmp, DEC);
+  }
+  ring.get(-1, (void *)&tmp);
+  Serial.print("  older element = ");
+  Serial.println(tmp, DEC);
   delay (9000);
 }
 
--- tests/eepromRingBufferTest.h
+++ tests/eepromRingBufferTest.h
@@ -1,5 +1,5 @@
 #define START_ADDR 256
-#define BUFFER_SZ 12
+#define BUFFER_SZ 6
 #define ENDURANCE 2
 #define DATA_SZ 2
 
