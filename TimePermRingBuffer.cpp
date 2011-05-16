c538747d0f4d4618364404d7de7c615a7a4c6158
Lorenzo Flueckiger <lorenzo@flupes.org>
2011-05-15 18:17:42 -0700

    fix bug in insert method

--- TimePermRingBuffer.cpp
+++ TimePermRingBuffer.cpp
@@ -31,7 +31,17 @@ bool TimePermRingBuffer::insert(DataSample &data, long current_time)
   Serial.print(" : delta=");
   Serial.println(delta, DEC);
 
-  if ( delta > m_period * m_dataSize ) {
+  if ( delta < 0 ) {
+    // something happened: we are going back in the past
+    // just let clear everything and start orer
+    Serial.println("------ back to the past -> clear");
+    clear();
+    m_lastTimeStamp.writeData((void*)&current_time);
+    push(data.data());
+    return true;
+  }
+
+  if ( delta > m_period * bufferSize() ) {
     // elapsed time greater than buffer time span
     Serial.println("------ elapsed time greater than buffer time span -> clear");
     clear();
@@ -39,17 +49,19 @@ bool TimePermRingBuffer::insert(DataSample &data, long current_time)
     push(data.data());
   }
   else {
-    steps = delta / m_period;
-    if ( steps > 0 ) {
-      // this is time to insert the new sample
-      if ( steps > 1 ) {
-        // elapsed time more than one single period
-        Serial.print("------ elapsed time more than a single period -> rotate steps = ");
-        Serial.println(steps-1, DEC);
-        rotate(steps-1);
-      }
-      m_lastTimeStamp.writeData((void*)&current_time);
-      push(data.data());
+    if ( 0 == delta % m_period ) {
+      steps = delta / m_period;
+      if ( steps > 0 ) {
+        // this is time to insert the new sample
+        if ( steps > 1 ) {
+          // elapsed time more than one single period
+          Serial.print("------ elapsed time more than a single period -> rotate steps = ");
+          Serial.println(steps-1, DEC);
+          rotate(steps-1);
+        }
+        m_lastTimeStamp.writeData((void*)&current_time);
+        push(data.data());
+      }      
     }
     else {
       Serial.println("------ period not elapsed -> no insertion");
@@ -64,7 +76,7 @@ long TimePermRingBuffer::read(int index, DataSample &data)
   get(index, (void*)data.data());
   long time;
   m_lastTimeStamp.readData((void*)&time);
-  time -= ( -index % bufferSize() ) * m_period;
+  time -= ( index % bufferSize() ) * m_period;
   return time;
 }
 
