0820e722b384cf73316d6eca45faa60832c779f5
Lorenzo Flueckiger <lorenzo@flupes.org>
2011-05-23 00:15:31 -0700

    simple program to clear the full eeprom

---
+++ tests/clearEeprom.cpp
@@ -0,0 +1,14 @@
+#include "SafeEeprom.h"
+
+#include "WProgram.h"
+
+int main(void)
+{
+  init();
+
+  for (uint16_t i=0; i<SafeEeprom::memSize(); i++) {
+    SafeEeprom::write_byte(i, 0xFF);
+  }
+
+  return 0;
+}
